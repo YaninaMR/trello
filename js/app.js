dc6755a7bf4880959f01c9e7553a339416cbd694
YaninaMR <mercadoyanic@gmail.com>
2017-11-22 12:05:39 -0500

    update trello

--- js/app.js
+++ js/app.js
@@ -1,45 +1,48 @@
-
+window.addEventListener('load', begin);
 
 var  inputadd = document.getElementsByClassName("js-add-list")[0].firstElementChild.children[1];
 var buttonadd = document.getElementsByClassName("js-add-list")[0].firstElementChild.children[2].firstElementChild;
 
-buttonadd.addEventListener('click',function(event){
+function addList(event){
 event.preventDefault();
-if(inputadd.value == '') {
-	alert("Por favor ingrese su lista esta vacio");
-	inputadd.focus();
-}
-else {
-
-		var  grandfather = document.getElementsByClassName("board")[0];
+if(event.target.parentElement.previousElementSibling.value) {
+	    
+		var  grandfather = event.target.parentElement.parentElement.parentElement.parentElement;
 		var  hijo1 = document.createElement("div");
 		var  hijo2 = document.createElement("div");
 		hijo1.setAttribute("class","red");
 		grandfather.appendChild(hijo1);
+		grandfather.insertBefore(hijo1,grandfather.firstElementChild);
 		hijo2.setAttribute("class","green");
 		grandfather.appendChild(hijo2);
+		grandfather.insertBefore(hijo2,grandfather.firstElementChild);
 		//var father = document.getElementsByClassName("green")[0];
 		var divhijo1 = document.createElement("div");
 		divhijo1.setAttribute("class","blue");
 		hijo1.appendChild(divhijo1);
-
+        
 		var  divhijo11 = document.createElement("div");
 		divhijo11.setAttribute("class","classdiv"); 
 		divhijo1.appendChild(divhijo11);
 
 		var h2 = document.createElement("h2");
 		h2.setAttribute("class","classh2");
-		var texth2 = document.createTextNode(inputadd.value);
+		var texth2 = document.createTextNode(event.target.parentElement.previousElementSibling.value);
 		h2.appendChild(texth2);
 		divhijo1.appendChild(h2);
-
+		//-----------------------*----------------------------
+		var divadd = document.createElement("div");
+		divadd.setAttribute("id","div-add");
+	    grandfather.appendChild(divadd); 
+	    grandfather.insertBefore(divadd,grandfather.firstElementChild);
+       //------------------------*----------------------------
 		var textarea = document.createElement("textarea");
 		textarea.setAttribute("class","classtextarea");
 		textarea.setAttribute("id","text-card");
           
 		//var texttextarea = document.createTextNode(inputadd.value);
 		//textarea.appendChild(texttextarea);
-		hijo2.appendChild(textarea);
+		divadd.appendChild(textarea);
 
 		var parraf = document.createElement("p");
 		parraf.setAttribute("class","classp");
@@ -75,21 +78,22 @@ else {
 		hijo2.appendChild(divhijo2);
 		hijo2.appendChild(ancodivhijo1);
 
-		var divadd = document.createElement("div");
+		
 		var divadd1 = document.createElement("div");
 		var divadd2 =  document.createElement("div");
-		divadd.setAttribute("id","div-add");
+		
 		divadd1.setAttribute("class","green");
 		divadd2.setAttribute("class","blue");
-		grandfather.appendChild(divadd);
+		
 		divadd.appendChild(divadd1);
 		divadd.appendChild(divadd2);
 		var input = document.createElement("input");
 		var ancoradd1 = document.createElement("a");
 		var ancoradd2 = document.createElement("a");
-		var textancoradd2 = document.createTextNode("X");
-		ancoradd2.appendChild(textancoradd2);
+		//var textancoradd2 = document.createTextNode("X");
+		//ancoradd2.appendChild(textancoradd2);
 		ancoradd2.setAttribute("href","#");
+		ancoradd2.setAttribute("class","fa fa-window-close-o");
 		input.setAttribute("type","submit");
 		input.setAttribute("value","Añadir");
 		input.setAttribute("id","add-card");
@@ -97,23 +101,32 @@ else {
 		divadd1.appendChild(input);
 		divadd1.appendChild(ancoradd1);
 		divadd2.appendChild(ancoradd2);
-
+         input.addEventListener("click", newTask);
 		//agregar un texto 
 			inputadd.value = '';
-	}
+
 //crear una nueva tarjeta 
-	var  textarea1 = document.getElementById("text-card");
+	
 //var  textarea1 = document.getElementsByClassName("board")[0].children[2].firstElementChild;
 	//var buttonaddCard = document.getElementsByClassName("board")[0].children[3].firstElementChild.firstElementChild;
-    var  buttonaddCard = document.getElementById("add-card");
-	buttonaddCard.addEventListener('click',function(e){
+    
+	 	}
+ }
 
-		if(textarea1.value == '') {
-			alert("Por favor ingrese su lista esta vacio");
-			textarea1.focus();
-		} else{
+ function begin() {
+  
+  buttonadd.addEventListener('click', addList);
+}
+
+
+//var  textarea1 = document.getElementById("text-card");
+function newTask(e){
+   e.preventDefault();
+ 
+	  if(e.target.parentElement.parentElement.firstElementChild.value) {
+				
 //var  divCardfather = document.getElementsByClassName("board")[0].children[3].firstElementChild;
-        var divCardfather = document.getElementById("div-add");                
+        var divCardfather = e.target.parentElement.parentElement;               
 		var ancorCard = document.createElement("a");
 		ancorCard.setAttribute("class","classancorCard");
 		ancorCard.setAttribute("href","/c/kp2JE5f1/8-tarjeta2");
@@ -127,16 +140,15 @@ else {
 		listDiv.setAttribute("class","classlistDiv");
 		ancorCard.appendChild(listDiv);
 		var span1 = document.createElement("span");
-		var textspan1 = document.createTextNode(textarea1.value);
+		var textspan1 = document.createTextNode(e.target.parentElement.parentElement.firstElementChild.value);
 		span1.appendChild(textspan1);
 		listDiv.appendChild(span1);
 		var span2 = document.createElement("span");
 		span1.appendChild(span2);
 
 		//agregar un texto 
-		textarea1.value = '';
-          }
-		});
- 	
- });
+		e.target.parentElement.parentElement.firstElementChild.value = '';
+          } 
+   }
 
+  
